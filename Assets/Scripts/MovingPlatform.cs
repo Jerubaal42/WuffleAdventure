3ef95da35ebe8dd4bbc2093a693623b7677ec615
Gideon Clegg <19.gideon.clegg@ksd.org>
2019-03-07 09:08:54 -0800

    Added moving platform

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovingPlatform : MonoBehaviour, IUpdate, IFixedUpdate
+public class MovingPlatform : MonoBehaviour, IUpdate
 {
     public bool smoothDampMove = false;
     public bool uniquePauses = false;
@@ -14,48 +14,60 @@ public class MovingPlatform : MonoBehaviour, IUpdate, IFixedUpdate
     public float[] moveTimes;
     public float waitTime = 0;
     public float moveTime = 1;
+    private bool wait = false;
     private int positionQuantity;
     private int location = 0;
+    private Vector3 currentVelocity;
+    private Vector2 prevPosition;
+    private float time = 0f;
 
     void Awake()
     {
         positionQuantity = positions.Length;
+        prevPosition = positions[positionQuantity - 1];
     }
 
     void Start()
     {
         GameController.gameController.updateList.Add(this);
-        GameController.gameController.fixedUpdateList.Add(this);
     }
 
     public void UpdateFunction()
     {
-        if (smoothDampMove)
+        if (wait)
         {
-
+            time += Time.deltaTime;
+            if (time > waitTime) { wait = false; time = 0; }
         }
         else
         {
-        }
-        if((positions[location] - (Vector2)transform.position).sqrMagnitude < 0.1f)
-        {
-            if(location == positionQuantity - 1)
+            if (smoothDampMove)
             {
-                if (backtrack) { backtrackDir = -1; }
-                else { location = 0; }
-            }
-            else if(backtrack && location == 0 && backtrackDir == -1)
-            {
-
+                transform.position = Vector3.SmoothDamp(transform.position, positions[location], ref currentVelocity, moveTime);
             }
             else
             {
-
+                time += Time.deltaTime;
+                transform.position = Vector3.Lerp(prevPosition, positions[location], time / moveTime);
+            }
+            if (smoothDampMove ? ((positions[location] - (Vector2)transform.position).sqrMagnitude < 0.1f) : (time / moveTime >= 1))
+            {
+                wait = true;
+                prevPosition = positions[location];
+                if (backtrack && location == 0 && backtrackDir == -1)
+                {
+                    backtrackDir = 1;
+                }
+                if (location == positionQuantity - 1)
+                {
+                    if (backtrack && backtrackDir == 1) { backtrackDir = -1; location += backtrackDir; }
+                    else { location = 0; }
+                }
+                else { location += backtrackDir; }
+                if (uniquePauses) { waitTime = waitTimes[location]; }
+                if (uniqueTravelTime) { moveTime = moveTimes[location]; }
+                time = 0;
             }
         }
-    }
-
-    public void FixedUpdateFunction()
-    {
     }
 }
